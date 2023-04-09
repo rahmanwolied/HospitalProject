d411306fba7fb59f32f47131415d57bbb74806b4
rahmanwolied <mosheur.r.wolied@gmail.com>
2023-04-09 06:52:37 +0600

    dbconnect

--- templates/header.php
+++ templates/header.php
@@ -1,27 +1,34 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PharmAir</title>
-    <!-- stylesheet -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="templates/style.css">
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</head>
+<?php 
+    include('config/dbconnect.php');
+?>
 
-<body class="grey lighten-4">
-    <div class="flex-container">
-        <div class="flex-item1">
-            <nav class="white">
-                <div class="nav-wrapper">
-                    <img src="assets/Group 1.svg" alt="icon">
-                    <a href="index.php" class="brand-logo left brand-text">PharmAir</a>
-                    <ul id="nav-mobile" class="right hide-on-small-and-down">
-                        <li><a href="login.php" class="grey-text text-darken-2">Login</a></li>
-                        <li><a href="register.php" class="grey-text text-darken-2">Register</a></li>
-                    </ul>
-                </div>
-            </nav>
-
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PharmAir</title>
+        <!-- stylesheet -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="templates/style.css">
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </head>
+    
+    <body class="grey lighten-4">
+        <div class="flex-container">
+            <div class="flex-item1">
+                <nav class="white">
+                    <div class="nav-wrapper">
+                        <img src="assets/Group 1.svg" alt="icon">
+                        <a href="index.php" class="brand-logo left brand-text">PharmAir</a>
+                        <ul id="nav-mobile" class="right hide-on-small-and-down">
+                            <li><a href="login.php" class="grey-text text-darken-2">Login</a></li>
+                            <li><a href="register.php" class="grey-text text-darken-2">Register</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                
+            </div>
+            

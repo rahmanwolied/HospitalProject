4b8d324c5435c5e4f7f26c11d424a241056da76b
rahmanwolied <mosheur.r.wolied@gmail.com>
2023-04-09 06:44:33 +0600

    iproved UI

--- index.php
+++ index.php
@@ -13,4 +13,40 @@
         </p>
     </div>
 
+    <div>
+        <a href="register.php" class="btn btn-full">
+            <span class="btn-text">Get Started</span>
+        </a>
+    </div>
 </main>
+
+<div class="search-section">
+    <div class="search-text">
+        <h1>
+            See if we have your medication.
+        </h1>
+        <p>
+            We have a wide variety of medications available for delivery.
+        </p>
+    </div>
+
+    <div class="search-form">
+        <div class="nav-wrapper">
+            <form>
+                <div class="input-field">
+                <input id="search" type="search" required>
+                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i class="material-icons">close</i>
+                </div>
+            </form>
+        </div>
+    </div>
+</div >
+
+<div class="products-div">
+    <h1>
+        Shop Common Medicines
+    </h1>
+
+</div>
+
--- templates/footer.php
+++ templates/footer.php
@@ -2,14 +2,18 @@
     <div class="footer">
         <footer class="page-footer">
             <div class="container">
+
                 <div class="row">
                     <div class="col l6 s12">
                         <a href="#" class="white-text" style="font-size: 1.5rem;">PharmAir</a>
-                        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                        <p class="grey-text text-lighten-4">PharmAir Home Delivery 4500 S Pleasant Valley Road, Suite 201 Austin, TX 78744-2911</p>
                     </div>
+
                     <div class="col l4 offset-l2 s12">
                         <ul>
-                            <li><a class="grey-text text-lighten-3" href="playground.php">Playground</a></li>
+                            <li><a class="grey-text text-lighten-3" href="index.php">Home</a></li>
+                            <li><a class="grey-text text-lighten-3" href="playground.php">Our Products</a></li>
+                            <li><a class="grey-text text-lighten-3" href="playground.php">Your Orders</a></li>
                         </ul>
                     </div>
                 </div>
